971cfbf374a932e9649c1957f302e133b237a8f8
Zach Wu <91336322+wu2022@users.noreply.github.com>
2021-11-03 17:54:44 -0500

    Update weibo_export.py

--- weibo_export.py
+++ weibo_export.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 
 # Change charset to utf8mb4
 engine = create_engine(
-    str(r"mysql+mysqldb://%s:" + '%s' + "@%s/%s?charset=utf8mb4") % ('root', 'Ss.768754763', '115.28.187.85',
-                                                                     'bi_db'))
+    str(r"mysql+mysqldb://%s:" + '%s' + "@%s/%s?charset=utf8mb4") % ('root', '***', '***',
+                                                                     '***'))
 sina1.to_sql(name='social_media', con=engine, if_exists='append', index=False)
 sina1.to_sql(name='social_media_backup', con=engine, if_exists='append', index=False)
 
